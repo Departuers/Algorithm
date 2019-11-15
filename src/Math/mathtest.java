08b2f6368142586177dc4d131dbc5a2b9205c486
Departures <2644631299@qq.com>
2019-11-15 13:25:54 +0800

    commit

---
+++ src/Math/mathtest.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class mathtest {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(3,3));
+    }
+
+    public static int ex(int a, int n) {
+        Math.pow(3,3);
+        return 1;
+    }
+}
