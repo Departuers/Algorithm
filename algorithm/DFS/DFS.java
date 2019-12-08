c093ffe0771a3fe09a883372a1d7af14af9ee59d
Departures <2644631299@qq.com>
2019-12-08 14:34:26 +0800

    commit

--- algorithm/DFS/DFS.java
+++ algorithm/DFS/DFS.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class DFS {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 7, 8, 6, 21, 13, 52};
-//        buFenHe(arr, 39, 0, new ArrayList<>());
-        int[] data=new int[6];
-        data[0]=1;
-        suShuHuan(6,data,1);
+        int[] arr = {1, 2, 3,15,23,13,12};
+//        buFenHe(arr, 46, 0, new ArrayList<>());
+//        int[] data = new int[6];
+//        data[0] = 1;
+//        suShuHuan(6,data,1);
     }
 
     /**
@@ -29,28 +29,25 @@ public class DFS {
      * 一条路走到完，发现没有找到解，回退，一步继续找
      *
      * @param arr 源数组
-     * @param k   剩余多少交给下次递归
+     * @param k   剩余多少交给下次递归,
      * @param cur 数组索引走到哪了
      */
     public static void buFenHe(int[] arr, int k, int cur, ArrayList<Integer> intS) {
-        if (k == 0) {
-            System.out.println("Yes");
+        if (k == 0) {//出口
             for (Integer anInt : intS) {
                 System.out.print(anInt + " ");
             }
             System.exit(0);
         }
-        if (k < 0 || cur == arr.length) return;
-        //这两个状态平行
-        buFenHe(arr, k, cur + 1, intS);//不要cur这个元素
+        if (k < 0 || cur == arr.length)//剪枝
+            return;
+        buFenHe(arr, k, cur + 1, intS);
         intS.add(arr[cur]);
-        int index = intS.size() - 1;
-        buFenHe(arr, k - arr[cur], cur + 1, intS);//要k这个元素
-        intS.remove(index);//回溯
+        buFenHe(arr, k - arr[cur], cur + 1, intS);
+        intS.remove(intS.size() - 1);
     }
 
     /**
-     *
      * @param n
      * @param arr
      * @param cur
@@ -61,23 +58,23 @@ public class DFS {
             return;
         }
         for (int i = 2; i <= n; i++) {
-            if (check(arr,i,cur)){
-                arr[cur]=i;
-                suShuHuan(n,arr,cur+1);
-                arr[cur]=0;
+            if (check(arr, i, cur)) {
+                arr[cur] = i;
+                suShuHuan(n, arr, cur + 1);
+                arr[cur] = 0;
             }
         }
     }
 
     private static void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + (i==arr.length-1?"":" "));
+            System.out.print(arr[i] + (i == arr.length - 1 ? "" : " "));
         }
         System.out.println();
     }
-
     /**
      * 判断是否
+     *
      * @param arr
      * @param k
      * @param cur
@@ -85,14 +82,16 @@ public class DFS {
      */
     private static boolean check(int[] arr, int k, int cur) {
         for (int i : arr) {
-            if (i==k||!isP(arr[cur-1]+k))return false;
+            if (i == k || !isP(arr[cur - 1] + k)) return false;
         }
         return true;
     }
+
     /**
      * 判断是否为素数
-     * @param k     需要判断是不是为素数的数字
-     * @return      b
+     *
+     * @param k 需要判断是不是为素数的数字
+     * @return b
      */
     private static boolean isP(int k) {
         for (int i = 2; i * i <= k; i++) {
@@ -101,4 +100,5 @@ public class DFS {
         }
         return true;
     }
+
 }
