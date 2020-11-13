96b0212fce85ae001de573c55042a30fca363f72
Departuers <2644631299@qq.com>
2020-11-13 14:36:33 +0800

    c

--- ACWing/src/night/大胖子走迷宫.java
+++ ACWing/src/night/大胖子走迷宫.java
@@ -1,6 +1,10 @@
 package night;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 /**
+ * https://www.acwing.com/solution/content/23766/
  * 小明是个大胖子，或者说是个大大胖子，如果说正常人占用 1 × 1 的面积，小明要占用 5 × 5 的面积。
  * 由于小明太胖了，所以他行动起来很不方便。当玩一些游戏时，小明相比
  * 小伙伴就吃亏很多。
@@ -26,16 +30,6 @@ package night;
  * +++++++++
  * +++++++++
  * +++++++++
- * 1
- * 2
- * 3
- * 4
- * 5
- * 6
- * 7
- * 8
- * 9
- * 10
  * 【样例输出】
  * 16
  * 【评测用例规模与约定】
@@ -45,6 +39,59 @@ package night;
  */
 public class 大胖子走迷宫 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            char[] t = sc.next().toCharArray();
+            for (int j = 1; j <= n; j++) {
+                g[i][j] = t[j - 1];
+            }
+        }
+        System.out.println(bfs());
+    }
 
+    static int k, n;
+    static char[][] g = new char[310][310];
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int[] d = {2, 1, 0}; //初始5*5然后3*3,最后1*1
+
+    static int bfs() {
+        ArrayDeque<node> q = new ArrayDeque<>();
+        q.add(new node(3, 3, 0));
+        st[3][3] = true;
+        while (!q.isEmpty()) {
+            node t = q.poll();
+            q.add(new node(t.x, t.y, t.time + 1));
+            for (int i = 0; i < 4; i++) {
+                int a = t.x + dir[i][0], b = t.y + dir[i][1];
+                int fat = t.time / k >= 2 ? 0 : d[t.time / k];
+                if (st[a][b]) continue;
+                if (a - fat < 1 || a + fat > n || b - fat < 1 || b + fat > n) continue;
+                boolean f = false;
+                for (int u = a - fat; u <= a + fat; u++) {
+                    for (int j = b - fat; j <= b + fat; j++) {
+                        if (g[u][j] == '*') f = true;
+                    }
+                }
+                if (f) continue;
+                if (a == n - 2 && b == n - 2) return t.time + 1;
+                st[a][b] = true;
+                q.add(new node(a, b, t.time + 1));
+            }
+        }
+        return -1;
+    }
+
+    static boolean[][] st = new boolean[310][310];
+
+    static class node {
+        int x, y, time;
+
+        public node(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
     }
 }
